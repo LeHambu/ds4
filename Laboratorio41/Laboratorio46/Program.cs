734efba46fe9072a47252aa3e11873952eff8e54
URIEL REYNA <uriel.reyna@utp.ac.pa>
2024-09-09 19:18:58 -0500

    lab4

---
+++ Laboratorio41/Laboratorio46/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Lab46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite el radio del circulo");
+            double radio = double.Parse(Console.ReadLine());
+
+            double area = Math.Pow(radio, 2) * Math.PI;
+
+            Console.WriteLine($"El area del ciculo es: {area}");
+        }
+    }
+}
