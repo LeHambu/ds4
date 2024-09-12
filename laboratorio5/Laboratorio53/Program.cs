7c10949f550af74aab34f2e8d71a24f11911b93b
URIEL REYNA <uriel.reyna@utp.ac.pa>
2024-09-11 19:09:51 -0500

    lab5

---
+++ laboratorio5/Laboratorio53/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Laboratorio5
+{
+    class Program
+    {
+        string[] frutas = { "manzana", "platano", "naranja" };
+        foreach(string fruta in frutas)
+            {
+            Console.WriteLine(fruta);
+            }
+    }
+}
---
+++ laboratorio5/Laboratorio56/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Laboratorio5
+{
+    class Program
+    {
+        Dictionary<string, string> paisesYCapitales = new Dictionary<string, string>
+        {
+            {"Francia", "Paris"},
+            {"España", "Madrid"},
+            {"Italia", "Roma"}
+        };
+        foreach (KeyValuePair<string, string> par in paisesYCapitales)
+            {
+            Console.WriteLine("La capital de " + par.Key +" es " + par.Value + ".");
+            }
+    }
+}
