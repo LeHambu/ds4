5c9779a5ed366432f83a322b0e5909f612397855
URIEL REYNA <uriel.reyna@utp.ac.pa>
2024-10-21 19:01:18 -0500

    Progreso al 50%

---
+++ Proyecto#1/Proyecto#1/Form1.Designer.cs
@@ -0,0 +1,362 @@
+﻿namespace Proyecto_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnClearEntry = new Button();
+            btnLimpiar = new Button();
+            btnDot = new Button();
+            btnIgual = new Button();
+            btnSuma = new Button();
+            btnResta = new Button();
+            btnMultiplicacion = new Button();
+            btnBorrar = new Button();
+            btnDivision = new Button();
+            btn0 = new Button();
+            btn3 = new Button();
+            btn2 = new Button();
+            btn1 = new Button();
+            btn6 = new Button();
+            btn4 = new Button();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn5 = new Button();
+            btnExponente = new Button();
+            btn9 = new Button();
+            btnRaizCuadrada = new Button();
+            txtResultado = new TextBox();
+            btnMostrarCalculos = new Button();
+            SuspendLayout();
+            // 
+            // btnClearEntry
+            // 
+            btnClearEntry.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnClearEntry.Location = new Point(153, 96);
+            btnClearEntry.Name = "btnClearEntry";
+            btnClearEntry.Size = new Size(75, 67);
+            btnClearEntry.TabIndex = 0;
+            btnClearEntry.Text = "CE";
+            btnClearEntry.UseVisualStyleBackColor = true;
+            // 
+            // btnLimpiar
+            // 
+            btnLimpiar.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnLimpiar.Location = new Point(264, 96);
+            btnLimpiar.Name = "btnLimpiar";
+            btnLimpiar.Size = new Size(75, 67);
+            btnLimpiar.TabIndex = 1;
+            btnLimpiar.Text = "C";
+            btnLimpiar.UseVisualStyleBackColor = true;
+            btnLimpiar.Click += btnLimpiar_Click_1;
+            // 
+            // btnDot
+            // 
+            btnDot.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnDot.Location = new Point(264, 519);
+            btnDot.Name = "btnDot";
+            btnDot.Size = new Size(75, 67);
+            btnDot.TabIndex = 2;
+            btnDot.Text = ".";
+            btnDot.UseVisualStyleBackColor = true;
+            // 
+            // btnIgual
+            // 
+            btnIgual.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnIgual.Location = new Point(371, 519);
+            btnIgual.Name = "btnIgual";
+            btnIgual.Size = new Size(75, 67);
+            btnIgual.TabIndex = 3;
+            btnIgual.Text = "=";
+            btnIgual.UseVisualStyleBackColor = true;
+            btnIgual.Click += btnIgual_Click_1;
+            // 
+            // btnSuma
+            // 
+            btnSuma.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnSuma.Location = new Point(371, 435);
+            btnSuma.Name = "btnSuma";
+            btnSuma.Size = new Size(75, 67);
+            btnSuma.TabIndex = 4;
+            btnSuma.Text = "+";
+            btnSuma.UseVisualStyleBackColor = true;
+            btnSuma.Click += btnSuma_Click;
+            // 
+            // btnResta
+            // 
+            btnResta.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnResta.Location = new Point(371, 349);
+            btnResta.Name = "btnResta";
+            btnResta.Size = new Size(75, 67);
+            btnResta.TabIndex = 5;
+            btnResta.Text = "-";
+            btnResta.UseVisualStyleBackColor = true;
+            btnResta.Click += btnResta_Click;
+            // 
+            // btnMultiplicacion
+            // 
+            btnMultiplicacion.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnMultiplicacion.Location = new Point(371, 266);
+            btnMultiplicacion.Name = "btnMultiplicacion";
+            btnMultiplicacion.Size = new Size(75, 67);
+            btnMultiplicacion.TabIndex = 6;
+            btnMultiplicacion.Text = "*";
+            btnMultiplicacion.UseVisualStyleBackColor = true;
+            btnMultiplicacion.Click += btnMultiplicacion_Click;
+            // 
+            // btnBorrar
+            // 
+            btnBorrar.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnBorrar.Location = new Point(371, 96);
+            btnBorrar.Name = "btnBorrar";
+            btnBorrar.Size = new Size(75, 67);
+            btnBorrar.TabIndex = 7;
+            btnBorrar.Text = "Del";
+            btnBorrar.UseVisualStyleBackColor = true;
+            // 
+            // btnDivision
+            // 
+            btnDivision.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnDivision.Location = new Point(371, 183);
+            btnDivision.Name = "btnDivision";
+            btnDivision.Size = new Size(75, 67);
+            btnDivision.TabIndex = 8;
+            btnDivision.Text = "/";
+            btnDivision.UseVisualStyleBackColor = true;
+            btnDivision.Click += btnDivision_Click;
+            // 
+            // btn0
+            // 
+            btn0.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn0.Location = new Point(153, 519);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(75, 67);
+            btn0.TabIndex = 9;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            btn0.Click += btn0_Click;
+            // 
+            // btn3
+            // 
+            btn3.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn3.Location = new Point(264, 435);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(75, 67);
+            btn3.TabIndex = 10;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
+            // 
+            // btn2
+            // 
+            btn2.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn2.Location = new Point(153, 435);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(75, 67);
+            btn2.TabIndex = 11;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn2_Click;
+            // 
+            // btn1
+            // 
+            btn1.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn1.Location = new Point(42, 435);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(75, 67);
+            btn1.TabIndex = 12;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // btn6
+            // 
+            btn6.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn6.Location = new Point(264, 349);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(75, 67);
+            btn6.TabIndex = 13;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn6_Click;
+            // 
+            // btn4
+            // 
+            btn4.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn4.Location = new Point(42, 349);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(75, 67);
+            btn4.TabIndex = 14;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn4_Click;
+            // 
+            // btn7
+            // 
+            btn7.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn7.Location = new Point(42, 266);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(75, 67);
+            btn7.TabIndex = 15;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btn7_Click;
+            // 
+            // btn8
+            // 
+            btn8.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn8.Location = new Point(153, 266);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(75, 67);
+            btn8.TabIndex = 16;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn8_Click;
+            // 
+            // btn5
+            // 
+            btn5.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn5.Location = new Point(153, 349);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(75, 67);
+            btn5.TabIndex = 17;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn5_Click;
+            // 
+            // btnExponente
+            // 
+            btnExponente.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnExponente.Location = new Point(153, 183);
+            btnExponente.Name = "btnExponente";
+            btnExponente.Size = new Size(75, 67);
+            btnExponente.TabIndex = 18;
+            btnExponente.Text = "^";
+            btnExponente.UseVisualStyleBackColor = true;
+            btnExponente.Click += btnExponente_Click;
+            // 
+            // btn9
+            // 
+            btn9.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn9.Location = new Point(264, 266);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(75, 67);
+            btn9.TabIndex = 19;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn9_Click;
+            // 
+            // btnRaizCuadrada
+            // 
+            btnRaizCuadrada.Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnRaizCuadrada.Location = new Point(264, 183);
+            btnRaizCuadrada.Name = "btnRaizCuadrada";
+            btnRaizCuadrada.Size = new Size(75, 67);
+            btnRaizCuadrada.TabIndex = 20;
+            btnRaizCuadrada.Text = "√ ";
+            btnRaizCuadrada.UseVisualStyleBackColor = true;
+            btnRaizCuadrada.Click += btnRaizCuadrada_Click;
+            // 
+            // txtResultado
+            // 
+            txtResultado.Font = new Font("Segoe UI", 26.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            txtResultado.Location = new Point(36, 11);
+            txtResultado.Name = "txtResultado";
+            txtResultado.Size = new Size(430, 54);
+            txtResultado.TabIndex = 21;
+            // 
+            // btnMostrarCalculos
+            // 
+            btnMostrarCalculos.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnMostrarCalculos.Location = new Point(42, 96);
+            btnMostrarCalculos.Name = "btnMostrarCalculos";
+            btnMostrarCalculos.Size = new Size(92, 154);
+            btnMostrarCalculos.TabIndex = 22;
+            btnMostrarCalculos.Text = "Mostrar Calculos";
+            btnMostrarCalculos.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(499, 609);
+            Controls.Add(btnMostrarCalculos);
+            Controls.Add(txtResultado);
+            Controls.Add(btnRaizCuadrada);
+            Controls.Add(btn9);
+            Controls.Add(btnExponente);
+            Controls.Add(btn5);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(btn4);
+            Controls.Add(btn6);
+            Controls.Add(btn1);
+            Controls.Add(btn2);
+            Controls.Add(btn3);
+            Controls.Add(btn0);
+            Controls.Add(btnDivision);
+            Controls.Add(btnBorrar);
+            Controls.Add(btnMultiplicacion);
+            Controls.Add(btnResta);
+            Controls.Add(btnSuma);
+            Controls.Add(btnIgual);
+            Controls.Add(btnDot);
+            Controls.Add(btnLimpiar);
+            Controls.Add(btnClearEntry);
+            Name = "Form1";
+            Text = "Calculadora";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnClearEntry;
+        private Button btnLimpiar;
+        private Button btnDot;
+        private Button btnIgual;
+        private Button btnSuma;
+        private Button btnResta;
+        private Button btnMultiplicacion;
+        private Button btnBorrar;
+        private Button btnDivision;
+        private Button btn0;
+        private Button btn3;
+        private Button btn2;
+        private Button btn1;
+        private Button btn6;
+        private Button btn4;
+        private Button btn7;
+        private Button btn8;
+        private Button btn5;
+        private Button btnExponente;
+        private Button btn9;
+        private Button btnRaizCuadrada;
+        private TextBox txtResultado;
+        private Button btnMostrarCalculos;
+    }
+}
---
+++ Proyecto#1/Proyecto#1/Form1.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Proyecto_1
+{
+    public partial class Form1 : Form
+
+    {
+        double num1 = 0, num2 = 0;
+        string operador = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Evento para los botones de los números
+        private void btnNumero_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            txtResultado.Text += boton.Text;  // Añade el número presionado al TextBox
+        }
+
+        // Evento para los botones de operación (+, -, *, /)
+        private void btnOperador_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            num1 = Convert.ToDouble(txtResultado.Text);  // Almacena el primer número
+            operador = boton.Text;  // Guarda el operador seleccionado (+, -, *, /)
+            txtResultado.Text = "";  // Limpia el TextBox para el segundo número
+        }
+
+        // Evento para el botón de igual
+        private void btnIgual_Click_1(object sender, EventArgs e)
+        {
+            num2 = Convert.ToDouble(txtResultado.Text);  // Captura el segundo número
+
+            // Realiza la operación correspondiente
+            switch (operador)
+            {
+                case "+":
+                    txtResultado.Text = (num1 + num2).ToString();
+                    break;
+                case "-":
+                    txtResultado.Text = (num1 - num2).ToString();
+                    break;
+                case "*":
+                    txtResultado.Text = (num1 * num2).ToString();
+                    break;
+                case "/":
+                    if (num2 != 0)
+                        txtResultado.Text = (num1 / num2).ToString();
+                    else
+                        txtResultado.Text = "Error: División por cero";
+                    break;
+                case "^":  // Potencia (exponente)
+                    double resultado = Math.Pow(num1, num2);
+                    txtResultado.Text = resultado.ToString();
+                    break;
+            }
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            txtResultado.Text += boton.Text;
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            txtResultado.Text += boton.Text;
+        }
+
+        private void btn3_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            txtResultado.Text += boton.Text;
+        }
+
+        private void btn4_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            txtResultado.Text += boton.Text;
+        }
+
+        private void btn5_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            txtResultado.Text += boton.Text;
+        }
+
+        private void btn6_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            txtResultado.Text += boton.Text;
+        }
+
+        private void btn7_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            txtResultado.Text += boton.Text;
+        }
+
+        private void btn8_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            txtResultado.Text += boton.Text;
+        }
+
+        private void btn9_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            txtResultado.Text += boton.Text;
+        }
+
+        private void btn0_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            txtResultado.Text += boton.Text;
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            num1 = Convert.ToDouble(txtResultado.Text);  // Almacena el primer número
+            operador = boton.Text;  // Guarda el operador seleccionado (+, -, *, /)
+            txtResultado.Text = "";
+        }
+
+        private void btnMultiplicacion_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            num1 = Convert.ToDouble(txtResultado.Text);  // Almacena el primer número
+            operador = boton.Text;  // Guarda el operador seleccionado (+, -, *, /)
+            txtResultado.Text = "";
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            num1 = Convert.ToDouble(txtResultado.Text);  // Almacena el primer número
+            operador = boton.Text;  // Guarda el operador seleccionado (+, -, *, /)
+            txtResultado.Text = "";
+        }
+
+        private void btnSuma_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            num1 = Convert.ToDouble(txtResultado.Text);  // Almacena el primer número
+            operador = boton.Text;  // Guarda el operador seleccionado (+, -, *, /)
+            txtResultado.Text = "";
+        }
+
+        private void btnExponente_Click(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            num1 = Convert.ToDouble(txtResultado.Text);  // Almacena el primer número
+            operador = boton.Text;  // Guarda el operador seleccionado (+, -, *, /)
+            txtResultado.Text = "";
+        }
+
+        private void btnRaizCuadrada_Click(object sender, EventArgs e)
+        {
+        
+            try
+            {
+                double num = Convert.ToDouble(txtResultado.Text);
+                if (num >= 0)
+                {
+                    double resultado = Math.Sqrt(num);
+                    txtResultado.Text = resultado.ToString();
+                }
+                else
+                {
+                    txtResultado.Text = "Error: No existe raíz cuadrada de número negativo.";
+                }
+            }
+            catch (FormatException)
+            {
+                txtResultado.Text = "Error: Entrada no válida.";
+            }
+
+        }
+
+        private void btnLimpiar_Click_1(object sender, EventArgs e)
+        {
+            txtResultado.Clear();  // Limpia el TextBox
+            num1 = 0;
+            num2 = 0;
+            operador = "";
+        }
+    }
+
+
+}
+
